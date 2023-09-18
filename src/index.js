6e71d951369553471ca53d18d4181ac9369d0d68
raaf17 <raafimuhammadd123@gmail.com>
2023-09-18 11:52:03 +0700

    menambahkan fitur crud yang dinamis

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
+require('dotenv').config()
 const express = require('express');
 const usersRoutes = require('./routes/Users');
 const middlewareLogRequest = require('./middleware/log');
 const app = express();
-const port = 3000;
+
+const PORT = process.env.PORT || 3000;
 
 // Middleware untuk mengurai body JSON dalam permintaan POST
 app.use(middlewareLogRequest.logRequest);
 app.use(express.json());
+app.use('/assets', express.static('public/images'));
 
 // Menggunakan middleware yang valid
 app.use('/users', usersRoutes);
+app.post('/upload', upload.single('photo'), (req, res) => {
+  res.json({
+    message: 'Upload berhasil'
+  });
+});
 
-app.listen(port, () => {
-  console.log(`Server berhasil di running di port ${port}`);
+app.use((error, req, res, next) => {
+  res.json({
+    message: error.message
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server berhasil di running di port ${PORT}`);
 });
---
+++ src/middleware/multer.js
@@ -0,0 +1,22 @@
+const multer = require('multer');
+const path = require('path');
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/images');
+  },
+  filename: (req, file, cb) => {
+    const timestamp = new Date().getTime();
+    const originalname = file.originalname;
+    // const extension = path.extname(file.originalname);
+    cb(null, `${timestamp}-${originalname}`);
+  }
+});
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 3 * 1000 * 1000
+  }
+});
+
+module.exports = upload;
